b3c428d65ef44d3bf8099e2e4f28a1412d7f9e6b
kirill.polyakov <kirillctc@gmail.com>
2022-12-21 22:03:07 +0600

    Initial Commit

---
+++ WildTour/WildTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildTourApp.swift
+//  WildTour
+//
+//  Created by Kirill Polyakov on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WildTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
